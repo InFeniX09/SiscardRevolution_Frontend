25ff7febe322d652ecde0b75f8fd64d4f7b34146
InFeniX09 <joseph.castillo@siscardperu.pe>
2024-03-23 14:01:50 -0500

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,8 @@
 export const environment = {
-  baseUrl: "http://localhost:3100"};
+  baseUrl: "http://localhost:3100",
+};
+/*
+ENV
+AUTH_SECRET=aea
+NEXTAUTH_URL=https://siscardrevolution.siscardperu.pe/api/auth 
+*/
