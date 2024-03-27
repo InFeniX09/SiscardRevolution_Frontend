e212174c157891a43d4cce3052d24f6ef51d7ff1
InFeniX09 <139170555+InFeniX09@users.noreply.github.com>
2024-03-27 14:35:26 -0500

    1

--- src/app/dashboard/centro-atencion/page.tsx
+++ src/app/dashboard/centro-atencion/page.tsx
@@ -6,7 +6,6 @@ import {
   DocumentIcon,
 } from "@heroicons/react/24/solid";
 import CardinfoComponent from "./ui/Cardinfo";
-import { getlistarTicketEstadoxFecha } from "@/src/actions/auth/centro-atencion";
 import SelectComponent from "@/src/components/ui/Select/Select";
 import CardComponent from "@/src/components/ui/Card/Card";
 import CardBlurComponent from "./ui/CardBlur";
