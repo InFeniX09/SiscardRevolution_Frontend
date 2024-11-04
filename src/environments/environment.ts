0d83c422d25f197c15a192b75f6dd14bac903a7c
Luis Condori Goicochea <lcondori@siscardperu.com>
2024-11-04 16:31:19 -0500

    chamba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: "http://172.17.7.43:3100",
-  NEXTAUTH_URL: "http://172.17.7.43:4000" 
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://172.17.7.43:4000'
 };
 /*
 ENV
